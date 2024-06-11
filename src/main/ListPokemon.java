c8cccda6ef2ccafc007234bbb0a2c92b5f9b2295
CongMinhLe <145794978+newbiecodexD@users.noreply.github.com>
2024-06-11 17:38:58 +0700

    Update ListPokemon.java

--- src/main/ListPokemon.java
+++ src/main/ListPokemon.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package main;
 
 import java.util.Random;
 
-/**
- *
- * @author Lê Minh
- */
+
 public class ListPokemon {
     private int no;
     private String name;
